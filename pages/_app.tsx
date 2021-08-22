a623e56c0d2840e7d2942fb6523b7c549ff4cf01
Bára Najmanová <bnajmanova@grifart.cz>
2021-08-22 12:59:40 +0200

    add styled components and basic styling

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html{
+  box-sizing: border-box;
+  background: #F5F4F0;
+  display:block;
+  height: 100%;
+  max-width: 640px;
+  margin:0 auto;
+  padding: 0;
+}
+
+body{
+  background-color:#fafafa;
+  min-height:100vh;
+  padding: 1rem;
+  margin-top:0;
+  font-family:Verdana;
+}
+`;
+
+const App = ({ Component, pageProps }: any) => {
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
+};
+
+export default App;
+
